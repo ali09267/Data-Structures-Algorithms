fb1c88ae7f3c800e1af08d38046a635c43a9a97f
Ali <aliahmedshaikh021@gmail.com>
2026-03-24 14:40:49 +0500

    Add Bottom View Problem of Binary Tree

--- src/String_Problems/GroupAnagrams.java
+++ src/String_Problems/GroupAnagrams.java
@@ -3,32 +3,16 @@ package String_Problems;
 import java.util.*;
 
 public class GroupAnagrams {
-//    public static List<List<String>> grpAnagrams(String[]strs){
-//        if (strs == null || strs.length == 0) return new ArrayList<>();
-//
-//        Map<String, List<String>> map = new HashMap<>();
-//        for (String s : strs) {
-//            char[] charArray = s.toCharArray();
-//            Arrays.sort(charArray);
-//            String key = new String(charArray);
-//            if (!map.containsKey(key)) {
-//                map.put(key, new ArrayList<>());
-//            }
-//            map.get(key).add(s);
-//        }
-//
-//        return new ArrayList<>(map.values());
-//    }
 
-    public static List<List<String>> grpAnagrams(String[]strs){
+    public static List<List<String>> grpAnagrams(String[]strs){//will return list of list of strs grouped together based on their anagramic behaviours
         if (strs == null || strs.length == 0) return new ArrayList<>();//edge case
         int n= strs.length;//length of strings array
         Map<String,List<String>>map=new HashMap<>();//for storing sorted str as key and original str as val
         List<List<String>>output=new ArrayList<>();//to store originals str (with same key) as groups whereas nested list is used for making groups inside list
 
         for(int i=0;i<n;i++){//main loop to iterate over array
-            char[]chars=strs[i].toCharArray();//convert each element of array to char
-            Arrays.sort(chars);//as sorting is based on characters not Strings
+            char[]chars=strs[i].toCharArray();//convert each element of array to char such as "eat"=>['e','a','t']
+            Arrays.sort(chars);//as sorting is based on characters not Strings such as ['e','a','t']=>['a','e','t']
 
             if(!map.containsKey(String.valueOf(chars))){//if map doesn't contain corresponding value based on curr key then make a new group
                 List<String>vals=new ArrayList<>();//new group/new list
